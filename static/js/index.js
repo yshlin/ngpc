291c2282de86124e04940621ebd16db9d5b08079
Eddie Lin <elin@mozilla.com>
2016-03-16 09:20:55 +0800

    First version of ngpc.tw website

---
+++ static/js/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+(function() {
+  var $sideNav = $('aside');
+  var $header = $('header');
+  var $scrollTip = $('#scroll-tip');
+  var sections = ['about', 'announcements', 'activities', 'contact'];
+  var tipContents = ['最新消息', '聚會訊息', '聯絡資訊'];
+  $.localScroll({duration: 200, lazy: true});
+  $(document.body).find('> section').waypoint({
+    handler: function(direction) {
+      var navIndex = sections.indexOf(this.element.id);
+      if ('up' == direction && navIndex > 0) {
+        navIndex--;
+      }
+      var currentSection = sections[navIndex];
+      $sideNav.find('a').attr('class', '');
+      $sideNav.find('#nav-' + currentSection).attr('class', 'active');
+      //$header.localScroll({target: '#'+currentSection+'-nav'});
+      $header.scrollTo(navIndex * 60, 100);
+      if (3 <= navIndex) {
+        $scrollTip.css('opacity', 0);
+      }
+      else {
+        $scrollTip.attr('href', '#' + sections[navIndex + 1]);
+        $scrollTip.css('opacity', 1);
+        $scrollTip.find('#tip-content').text(tipContents[navIndex]);
+      }
+    },
+    offset: 60
+  });
+})();
