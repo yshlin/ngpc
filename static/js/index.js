b0a092a7bbf3e57e5c7d5e2c08f5fda7dca4b8ea
Max Huang <huang.fu.chang@gmail.com>
2020-09-05 21:45:41 +0800

    Add live cam & dedication information.

--- static/js/index.js
+++ static/js/index.js
@@ -5,8 +5,8 @@
   var $header = $('header');
   var $scrollTip = $('#scroll-tip');
   //var sections = ['about', 'announcements', 'activities', 'contact'];
-  var sections = ['about', 'contact'];
-  var tipContents = ['聯絡資訊'];//['最新消息', '聚會訊息', '聯絡資訊'];
+  var sections = ['about', 'live', 'contact'];
+  var tipContents = ['禮拜直播', '聯絡資訊'];//['最新消息', '聚會訊息', '聯絡資訊'];
   $.localScroll({duration: 200, lazy: true});
   $(document.body).find('> section').waypoint({
     handler: function(direction) {
